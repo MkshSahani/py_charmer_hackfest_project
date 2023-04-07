214a3c068410c5ca84da2ea1a27a36edb6867c84
MkshSahani <msco18333@gmail.com>
2023-04-08 05:09:55 +0530

    desktop app updated

---
+++ pycharmer_project_desktop_app/pycharmer_project_desktop_app/config/constants.py
@@ -0,0 +1,3 @@
+TOPBAR_TITLE = "Online Work Buddy"
+STUDENT_LOGIN_TITLE = "Student Login"
+STUDENT_REG_TITLE   = "Student Registration"
---
+++ pycharmer_project_desktop_app/pycharmer_project_desktop_app/test.py
@@ -0,0 +1,5 @@
+from gui_components.registration_component.student_reg_comp import StudentRegistrationComponent
+import customtkinter
+customtkinter.set_appearance_mode("dark")
+g = StudentRegistrationComponent()
+g.render()
